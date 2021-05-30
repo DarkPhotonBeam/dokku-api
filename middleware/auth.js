d401c01bfcdc3b07232ca28fff1f17c0e6bbcddd
Daniel Leuthold <danphoton@yahoo.com>
2021-05-30 19:06:06 +0200

    Auth

---
+++ middleware/auth.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const redis = require('redis');
+const REDIS_PORT = process.env.REDIS_PORT;
+const REDIS_IP = process.env.REDIS_IP;
+const client = redis.createClient(REDIS_PORT, REDIS_IP);
+const { promisify } = require('util');
+const getAsync = promisify(client.get).bind(client);
+
+module.exports = async (req, res, next) => {
+    try {
+        const key = await getAsync('API-KEY');
+        const secret = await getAsync('API-SEC');
+
+        const keyHeader = req.header('API-KEY');
+        const secretHeader = req.header('API-SEC');
+
+        if (key && secret && key === keyHeader && secret === secretHeader)
+        {
+            next();
+        } else {
+            throw 'Invalid Headers';
+        }
+    } catch {
+        res.status(403).json({
+            error: 'Forbidden'
+        });
+    }
+};
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ require('dotenv').config();
 const app = require('express')();
 const port = process.env.PORT || 8080;
 const bodyParser = require('body-parser');
+const auth = require('../middleware/auth');
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello There');
+app.use(auth);
+
+app.get('/secret-endpoint', async (req, res) => {
+    res.status(200).json({coolData: 'very cool Data'});
 });
 
 app.listen(port, () => {
