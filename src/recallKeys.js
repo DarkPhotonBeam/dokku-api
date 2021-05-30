9aefedff6e774b2befe2fcf5bbe7bf2fb8e603bc
Daniel Leuthold <danphoton@yahoo.com>
2021-05-30 19:30:21 +0200

    Recall Keys Script

---
+++ src/recallKeys.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const redis = require('redis');
+const client = redis.createClient(process.env.REDIS_URL);
+const { promisify } = require('util');
+const getAsync = promisify(client.get).bind(client);
+
+(async () => {
+    const key = await getAsync('API-KEY');
+    const secret = await getAsync('API-SEC');
+
+    console.log(`Public Key: ${key}`);
+    console.log(`Secret Key: ${secret}`);
+})();
