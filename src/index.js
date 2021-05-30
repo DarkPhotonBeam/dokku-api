97f0c70fb0c53ee24eadabdd1cb04d0c833aa41e
Daniel Leuthold <danphoton@yahoo.com>
2021-05-30 19:52:10 +0200

    Added apps:list and ps:inspect endpoints

--- src/index.js
+++ src/index.js
@@ -3,13 +3,30 @@ const app = require('express')();
 const port = process.env.PORT || 8080;
 const bodyParser = require('body-parser');
 const auth = require('../middleware/auth');
+const execAsync = require('../lib/execAsync');
 
 app.use(bodyParser.json());
 
 app.use(auth);
 
-app.get('/secret-endpoint', async (req, res) => {
-    res.status(200).json({coolData: 'very cool Data'});
+app.get('/apps/list', async (req, res) => {
+    try {
+        const res = await execAsync('dokku --quiet apps:list');
+        const appList = res.split('\n');
+        res.status(200).json({apps: appList});
+    } catch (error) {
+        res.status(500).json({error});
+    }
+});
+
+app.get('/ps/inspect', async (req, res) => {
+    try {
+        const res = await execAsync(`dokku ps:inspect ${req.params.app}`);
+        const appList = res.split('\n');
+        res.status(200).json({apps: appList});
+    } catch (error) {
+        res.status(500).json({error});
+    }
 });
 
 app.listen(port, () => {
